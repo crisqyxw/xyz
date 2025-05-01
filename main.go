28756fa2e32ee399b93f705ade999a187c980180
Cris.Q <me@crisq.top>
2025-05-01 14:14:33 +0800

    添加基础的缓存功能

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/ultrazg/xyz/service"
+	"github.com/ultrazg/xyz/utils"
 	"log"
 )
 
 func main() {
+	utils.InitCache()
 	err := service.Start()
 	if err != nil {
 		log.Fatal(err)
---
+++ utils/cache.go
@@ -0,0 +1,104 @@
+// utils/cache.go
+package utils
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+)
+
+var RedisClient *redis.Client
+var Ctx = context.Background()
+
+func InitCache() {
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+}
+
+// GetCacheKey 生成缓存键
+func GetCacheKey(uri string, token string) string {
+	hash := sha256.Sum256([]byte(uri + ":" + token))
+	return hex.EncodeToString(hash[:])
+}
+
+// GetCachedResponse 获取缓存内容
+func GetCachedResponse(key string) (string, error) {
+	return RedisClient.Get(Ctx, key).Result()
+}
+
+// SetCachedResponse 设置缓存内容并存储时间戳
+// SetCachedResponse 设置缓存内容并存储时间戳
+// SetCachedResponse 设置缓存内容并存储时间戳
+func SetCachedResponse(key string, response string, ttl int) {
+	now := time.Now().UTC().Format("Mon, 02 Jan 2006 15:04:05 GMT") // 强制使用 GMT 格式
+	RedisClient.Set(Ctx, key, response, 0)
+	RedisClient.Set(Ctx, key+":last_modified", now, 0)
+}
+
+// WithConditionalGet 修改后的函数
+func WithConditionalGet(handler gin.HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 构造缓存键
+		rawURI := c.Request.URL.Path
+		token := c.Request.Header.Get("x-jike-access-token")
+		cacheKey := GetCacheKey(rawURI, token)
+
+		// 获取缓存值
+		cached, err := GetCachedResponse(cacheKey)
+
+		// 如果启用调试可记录日志
+		log.Printf("Cache Key: %s | Hit: %v", cacheKey, err == nil)
+		log.Printf("Cache error message: %s", err)
+
+		// 缓存存在，添加 Last-Modified Header 并返回缓存的响应数据
+		if err == nil {
+			// 获取缓存的存储时间
+			lastModified, err := RedisClient.Get(Ctx, cacheKey+":last_modified").Result()
+			if err == nil {
+				// 验证时间格式是否符合 RFC1123 或 GMT
+				_, err := time.Parse("Mon, 02 Jan 2006 15:04:05 GMT", lastModified)
+				if err == nil {
+					c.Header("Last-Modified", lastModified)
+				} else {
+					log.Printf("Invalid Last-Modified format: %s", lastModified)
+				}
+			}
+
+			// 返回缓存的响应数据
+			c.Data(200, "application/json", []byte(cached))
+			return
+		}
+
+		// 缓存未命中，继续执行 handler 并捕获响应
+		writer := &responseWriter{body: &bytes.Buffer{}, ResponseWriter: c.Writer}
+		c.Writer = writer
+
+		handler(c)
+
+		responseBody := writer.body.String()
+
+		// 更新缓存
+		SetCachedResponse(cacheKey, responseBody, 5*60) // TTL 5分钟
+
+		// 不需要重写响应，因为已经由 writer 写出
+	}
+}
+
+type responseWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w *responseWriter) Write(data []byte) (int, error) {
+	w.body.Write(data)
+	return w.ResponseWriter.Write(data)
+}
